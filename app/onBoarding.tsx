71632f916128dda8fb82a0b2d91676f98df89485
srikarIsMissing <srkarrallabhandi.16@outlook.com>
2025-07-01 00:31:42 +0530

    addes tailwind

---
+++ app/onBoarding.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import '../main.css';
+import {
+    View,
+    Text
+} from 'react-native';
+
+export default  function App() {
+    return (
+        <View>
+            <Text className="font-semibold text-white text-5xl">Hello world</Text>
+        </View>
+    )
+}
